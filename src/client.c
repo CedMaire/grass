c1fe7ea1e0d5fcd31ec337f05de8600784a45e97
Cedric Maire <mairecedric@netplus.ch>
2019-03-03 14:28:47 +0100

    Added basic server-client communicaton

--- src/client.c
+++ src/client.c
@@ -37,4 +37,44 @@ int main(int argc, char **argv) {
     // TODO:
     // Make a short REPL to send commands to the server
     // Make sure to also handle the special cases of a get and put command
+
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd == -1) {
+        fprintf(stderr, "Socket creation failed!\n");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Socket created...\n");
+    }
+
+    struct sockaddr_in server_address;
+    bzero(&server_address, sizeof(server_address));
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_address.sin_port = htons(6969);
+
+    if (connect(socket_fd, (struct sockaddr*) &server_address, sizeof(server_address)) != 0) {
+        fprintf(stderr, "Connection failed!\n");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Connected...\n");
+    }
+
+    char *string_exit = "exit\n";
+    while(true) {
+        char buffer[16];
+        bzero(buffer, sizeof(buffer));
+        write(socket_fd, string_exit, strlen(string_exit) + 1);
+        read(socket_fd, buffer, sizeof(buffer));
+        printf("From server: %s\n", buffer);
+        if (strncmp(buffer, string_exit, strlen(string_exit)) == 0) {
+            printf("Exit...\n");
+            break;
+        }
+    }
+
+    fflush(stdout);
+
+    close(socket_fd);
+    exit(EXIT_SUCCESS);
 }
--- src/server.c
+++ src/server.c
@@ -54,4 +54,59 @@ int main() {
     // TODO:
     // Parse the rass.conf file
     // Listen to the port and handle each connection
+
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd == -1) {
+        fprintf(stderr, "Socket creation failed!\n");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Socket created...\n");
+    }
+
+    struct sockaddr_in server_address;
+    bzero(&server_address, sizeof(server_address));
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(6969);
+
+    if (bind(socket_fd, (struct sockaddr *) &server_address, sizeof(server_address)) != 0) {
+        fprintf(stderr, "Socket binding failed!\n");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Socket binded...\n");
+    }
+
+    if (listen(socket_fd, 5) != 0) {
+        fprintf(stderr, "Listen failed!\n");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Listening...\n");
+    }
+
+    struct sockaddr_in client_address;
+    bzero(&client_address, sizeof(client_address));
+    int client_struct_length = sizeof(client_address);
+    int connection_fd = accept(socket_fd, (struct sockaddr *) &client_address, &client_struct_length);
+    if (connection_fd < 0) {
+        fprintf(stderr, "Accepting failed!\n");
+    } else {
+        printf("Client accepted...\n");
+    }
+
+    char *string_exit = "exit\n";
+    while(true) {
+        char buffer[16];
+        bzero(buffer, sizeof(buffer));
+        read(connection_fd, buffer, sizeof(buffer));
+        printf("From client: %s\n", buffer);
+        if (strncmp(buffer, string_exit, strlen(string_exit)) == 0) {
+            write(connection_fd, string_exit, strlen(string_exit) + 1);
+            printf("Exit...\n");
+            break;
+        }
+    }
+
+    fflush(stdout);
+    close(socket_fd);
 }
