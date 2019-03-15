be425d577425057ddbe0f6c0032b2f02ece4c2d2
BenjaminDeleze <benjamin.deleze@epfl.ch>
2019-03-15 23:09:10 +0100

    implementation of parse_grass near complete

--- src/server.c
+++ src/server.c
@@ -6,11 +6,11 @@ static int numUsers;
 static struct Command **cmdlist;
 static int numCmds;
 char port[7] = "31337";
+static char* base[32];
 
 // Helper function to run commands in unix.
-void run_command(const char* command, int sock){
+void run_command(const char* command, int sock) {
 }
-
 
 /*
  * Send a file to the client as its own thread
@@ -43,48 +43,120 @@ void *connection_handler(void* sockfd) {
  * Output: A line seperated list of matching files' addresses
  */
 void search(char *pattern) {
-    // TODO
+	// TODO
 }
 
 // Parse the grass.conf file and fill in the global variables
 void parse_grass() {
+	FILE* conf;
+	if ((conf = fopen("grass.conf", "r")) == NULL) {
+		fprintf(stderr, "grass.conf not found\n");
+	} else {
+		printf("File loaded\n");
+	}
+
+	char* line[32];
+	size_t comment_lines = 0;
+	size_t nbr_users = 0;
+	size_t offset_of_users = 0;
+	while (fgets(line, 32, conf) != NULL) {
+		char* token = strtok(line, " ");
+		if (strncmp("#", token, 1) == 0) {
+			comment_lines++;
+		//Parse the base
+		} else if (comment_lines == 3 && strncmp("base", token, 4) == 0) {
+			token = strtok(NULL, " ");
+			if (token == NULL) {
+				fprintf(stderr, "base not found\n");
+			}
+			strncpy(base, token, 32);
+			printf("base: %s\n", token);
+		//Parse the port
+		} else if (comment_lines == 4 && strncmp("port", token, 4) == 0) {
+			token = strtok(NULL, " ");
+			if (token == NULL) {
+				fprintf(stderr, "port not found\n");
+			}
+			strncpy(port, token, 7);
+			printf("port: %s\n", token);
+		//Count the number of user
+		} else if (comment_lines == 5 && strncmp("user", token, 4) == 0) {
+			if (nbr_users == 0) {
+				offset_of_users = ftell(conf);
+			}
+			nbr_users++;
+		}
+	}
+	//Set the offset just before the users
+	if (fseek(conf, offset_of_users - 32, SEEK_SET) != 0) {
+		fprintf(stderr, "error when counting the users\n");
+	}
+
+	if (userlist = calloc(nbr_users, sizeof(struct User*)) == NULL) {
+		fprintf(stderr, "allocation error\n");
+	}
+	//Fill the userlist
+	while (fgets(line, 32, conf) != NULL) {
+		char* token = strtok(line, " ");
+		if (User u = calloc(1, sizeof(struct User)) == NULL) {
+			fprintf(stderr, "allocation error\n");
+		}
+		if (comment_lines == 5 && strncmp("user", token, 4) == 0) {
+			token = strtok(NULL, " ");
+			if (token == NULL) {
+				fprintf(stderr, "user not found, wrong format\n");
+			}
+			printf("user: %s ", token);
+
+			token = strtok(NULL, " ");
+			if (token == NULL) {
+				fprintf(stderr, "password not found, wrong format\n");
+			}
+			printf("pwd: %s\n", token);
+		}
+	}
+
+	printf("End of conf file reached\n");
 }
 
 int main() {
-    // TODO:
-    // Parse the rass.conf file
-    // Listen to the port and handle each connection
 
-    int socket_fd = create_socket(server);
-    if (socket_fd < 0) {
-        fprintf(stderr, "Exit program because of error.\n");
-    }
+	parse_grass();
+	// TODO:
+	// Parse the grass.conf file
+	// Listen to the port and handle each connection
 
-    struct sockaddr_in client_address;
-    bzero(&client_address, sizeof(client_address));
-    int client_struct_length = sizeof(client_address);
-    int connection_fd = accept(socket_fd, (struct sockaddr *) &client_address, &client_struct_length);
-    if (connection_fd < 0) {
-        fprintf(stderr, "Accepting failed!\n");
-    } else {
-        printf("Client accepted...\n");
-    }
+	int socket_fd = create_socket(server);
+	if (socket_fd < 0) {
+		fprintf(stderr, "Exit program because of error.\n");
+	}
 
-    char *string_exit = "exit\n";
-    while(true) {
-        char buffer[16];
-        bzero(buffer, sizeof(buffer));
-        read(connection_fd, buffer, sizeof(buffer));
-        printf("From client: %s\n", buffer);
-        if (strncmp(buffer, string_exit, strlen(string_exit)) == 0) {
-            write(connection_fd, string_exit, strlen(string_exit) + 1);
-            printf("Exit...\n");
-            break;
-        }
-    }
+	struct sockaddr_in client_address;
+	bzero(&client_address, sizeof(client_address));
+	int client_struct_length = sizeof(client_address);
+	int connection_fd = accept(socket_fd, (struct sockaddr *) &client_address,
+			&client_struct_length);
+	if (connection_fd < 0) {
+		fprintf(stderr, "Accepting failed!\n");
+	} else {
+		printf("Client accepted...\n");
+	}
 
-    fflush(stdout);
-    close(socket_fd);
+	char *string_exit = "exit\n";
+	while (true) {
+		char buffer[16];
+		bzero(buffer, sizeof(buffer));
+		read(connection_fd, buffer, sizeof(buffer));
+		printf("From client: %s\n", buffer);
+		if (strncmp(buffer, string_exit, strlen(string_exit)) == 0) {
+			write(connection_fd, string_exit, strlen(string_exit) + 1);
+			printf("Exit...\n");
+			break;
+		}
+	}
 
-    exit(EXIT_SUCCESS);
+	fflush(stdout);
+	close(socket_fd);
+
+	exit(EXIT_SUCCESS);
 }
